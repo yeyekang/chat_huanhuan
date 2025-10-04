bf6729b2ac595366c0d43336c37ab92bea5df4f0
yeyekang <15249185085@163.com>
2025-10-04 20:57:06 +0800

    update

--- model_download.py
+++ model_download.py
@@ -1,5 +1,9 @@
-import torch
-from modelscope import snapshot_download, AutoModel, AutoTokenizer
-import os
+from modelscope import snapshot_download
 
-model_dir = snapshot_download('LLM-Research/Meta-Llama-3.1-8B-Instruct', cache_dir='/root/autodl-tmp', revision='master')
+model_dir = snapshot_download(
+    model_id='LLM-Research/Meta-Llama-3.1-8B-Instruct',
+    cache_dir='autodl-tmp/chat-huanhuan/model',
+    revision='master'
+)
+
+print("✅ 下载完成，目录：", model_dir)
